3c9927a98cd96682c4732b3fa35200573bad01fe
CJW23 <cjw7242@gmail.com>
2023-09-11 00:55:43 +0900

    프로젝트 구성

---
+++ chatting-api/src/main/java/com/cjw/chatting/ChattingApiApplication.java
@@ -0,0 +1,12 @@
+package com.cjw.chatting;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingApiApplication.class);
+    }
+}
---
+++ core/src/main/java/com/cjw/chatting/service/TestService.java
@@ -0,0 +1,10 @@
+package com.cjw.chatting.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    public String test() {
+        return "awd";
+    }
+}
