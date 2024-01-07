6905fc7e2db740735e5a73e09a4160da7c3e6b15
CJW23 <cjw7242@gmail.com>
2024-01-07 19:24:55 +0900

    - Spring Security OAuth2 작업중

---
+++ chatting-stream-server/src/main/java/com/cjw/chatting/config/security/ChattingOAuth2UserService.java
@@ -0,0 +1,18 @@
+package com.cjw.chatting.config.security;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChattingOAuth2UserService extends DefaultOAuth2UserService {
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        //custom한 로직 작성
+
+        return oAuth2User;
+    }
+}
--- chatting-stream-server/src/main/java/com/cjw/chatting/config/security/SecurityConfig.java
+++ chatting-stream-server/src/main/java/com/cjw/chatting/config/security/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.cjw.chatting.config.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+@RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+    private final ChattingOAuth2UserService chattingOAuth2UserService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -17,14 +22,23 @@ public class SecurityConfig {
                         a.anyRequest().permitAll())
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable);
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .oauth2Login(config -> {
+                    config
+                            .loginPage("/login")
+                            .userInfoEndpoint(userInfoEndpointConfig ->
+                                    userInfoEndpointConfig.userService(chattingOAuth2UserService))
+                            .successHandler(successHandler())
+                    ;  //커스텀 userService 등록
+
+                });
+
         return http.build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(null);
-        return authenticationManagerBuilder.build();
+    public AuthenticationSuccessHandler successHandler() {
+        return ((request, response, authentication) -> {
+            //response.sendRedirect("https://naver.com");
+        });
     }
 }
